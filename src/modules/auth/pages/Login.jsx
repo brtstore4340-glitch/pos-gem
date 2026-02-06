ba4bf5cf5bebf674bf4361748bc544f28eb6baf4
brtstore4340-glitch <brtstore.4340@gmail.com>
2026-02-07 03:49:36 +0700

    fix: remove invalid firebase config imports causing runtime crash

--- src/modules/auth/pages/Login.jsx
+++ src/modules/auth/pages/Login.jsx
@@ -1,3 +1,2 @@
-﻿import { auth, db } from './src/firebase/config.js';
-import LoginPage from "../../../pages/LoginPage";
+﻿import LoginPage from "../../../pages/LoginPage";
 export default LoginPage;
